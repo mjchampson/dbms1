2dcc6ed779976ac5e55a9ee14f532405fec8d1bd
mjchampson <mjsamson35@yahoo.com>
2017-03-01 09:27:50 +0800

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,5 +1,5 @@
 set echo on
-connect system/amakal
+connect system / Kal
 
 ---create PHP Application User
 drop phpuser cascade;
@@ -24,8 +24,25 @@ category varchar2(20),
 name varchar2(20),
 
 insert into parts values(1,'electrical','lamp');
-insert into parts values(1,'electrical','lamp');
-insert into parts values(1,'electrical','lamp');
-insert into parts values(1,'electrical','lamp');
-insert into parts values(1,'electrical','lamp');
-insert into parts values(1,'electrical','lamp');
+insert into parts values(2,'electrical','wire');
+insert into parts values(3,'electrical','switch');
+insert into parts values(4,'plumbing','pump');
+insert into parts values(5,'plumbing','sink');
+insert into parts values(6,'plumbing','toilet');
+commit;
+
+connect php_sec_admin/welcome;
+--Authentication table with the webuser usernames & password.
+-- A real application would NEVER store plain.txt passwords.
+-- but this code is a demo for user of client identifiers & 
+-- not about authentication
+
+create table_php authentication
+(app_username varchar2(20) primary key,
+app_password varchar2(20) not null);
+
+insert into php_authentication values('mirana','tiger');
+insert into php_authentication values('luna','leopard');
+commit;
+
+grant select on php_authentication to phpuser;
