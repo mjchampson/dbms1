a5466f36d812136f394c3e68bc2f694573ef826a
mjchampson <mjsamson35@yahoo.com>
2017-03-01 10:04:12 +0800

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -2,15 +2,15 @@ set echo on
 connect system / amakal
 
 ---create PHP Application User
-drop phpuser cascade;
-create phpuser identified by welcome;
-grant connect, resource ot phpuser;
+drop user phpuser cascade;
+create user phpuser identified by welcome;
+grant connect, resource to phpuser;
 alter user phpuser default tablespace users
 temporary tablespace temp account unlock;
 
 ---Create user owner security info about the application
-drop php_sec_admin cascade;
-create php_sec_admin identified by welcome;
+drop user php_sec_admin cascade;
+create user php_sec_admin identified by welcome;
 alter user php_sec_admin default tablespace system
 temporary tablespace temp account unlock;
 grant create procedure , create session , create table , resource,
@@ -21,7 +21,7 @@ connect phpuser/welcome;
 create table parts 
 (id number primary key ,
 category varchar2(20),
-name varchar2(20),
+name varchar2(20));
 
 insert into parts values(1,'electrical','lamp');
 insert into parts values(2,'electrical','wire');
@@ -33,11 +33,11 @@ commit;
 
 connect php_sec_admin/welcome;
 --Authentication table with the webuser usernames & password.
--- A real application would NEVER store plain.txt passwords.
--- but this code is a demo for user of client identifiers & 
--- not about authentication
+--A real application would NEVER store plain.txt passwords.
+--but this code is a demo for user of client identifiers & 
+--not about authentication
 
-create table_php authentication
+create table php_authentication
 (app_username varchar2(20) primary key,
 app_password varchar2(20) not null);
 
